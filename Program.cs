103c1716f828da294f66c1710f7b6955126120fa
winterednalynn <ednalynnnl@gmail.com>
2024-01-07 19:49:40 -0800

    Name CS

--- Program.cs
+++ Program.cs
@@ -11,6 +11,10 @@ namespace GA_GitHub_Basics_Ednalynn
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Professor");
+
+            Console.WriteLine("Edna Lynn");
+            Console.WriteLine("I initally chose computer science because I wanted an extended knowledge of the design of programming development. Thus far I've learned problem solving" +
+                "skills, logic, reasoning and attention to detail. ");
         }
     }
 }
